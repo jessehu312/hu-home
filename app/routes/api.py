104768285f2dc5f8c811e029258c51397aca01cf
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2020-12-31 23:23:11 -0800

    [PROJ-68] Setup server verification and fetching (#5)

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app import settings
+from firebase_admin import auth
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 @blueprint.route('/client-config')
 def client_config():
     return settings.FIREBASE_CLIENT_CONFIG
+
+@blueprint.route('/get-user', methods=['POST'])
+def get_user():
+    id_token = request.json.get('token')
+    decoded_token = auth.verify_id_token(id_token)
+    uid = decoded_token['uid']
+    return decoded_token, 200
