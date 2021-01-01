b8b757f0d67aa0285d13f41471d752ee45195a24
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-01-01 14:51:17 -0800

    [PROJ-69] Fixed duplicate emails and Add Accounts (#6)
    * Fixed duplicate emails
    
    * Fixed comments

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request
 from app import settings
 from firebase_admin import auth
+from app.models.user import User
+from app.database import db
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 
@@ -11,9 +13,21 @@ def client_config():
         'radar': settings.RADAR_PUBLISHABLE_KEY
     }
 
+def list_users():
+    return list(map(lambda row: {'id': row.id, 'username': row.email, 'firebase_id': row.firebase_id}, User.query.all()))
+
+def add_user(id, email):
+    if not User.query.filter_by(firebase_id=id).first():
+        user = User(username = email, firebase_id = id)
+        db.session.add(user)
+        db.session.commit()
+    return list_users()
+
 @blueprint.route('/get-user', methods=['POST'])
 def get_user():
     id_token = request.json.get('token')
     decoded_token = auth.verify_id_token(id_token)
     uid = decoded_token['uid']
+    email = decoded_token['email']
+    decoded_token['listUsers'] = add_user(uid, email)
     return decoded_token, 200
